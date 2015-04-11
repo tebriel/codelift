8b269f377addca82cfa99cef6bfaaf3dd182d0ed
Chris Moultrie <chris@moultrie.org>
2015-04-11 10:26:39 -0400

    More tests, better operation

--- elevator.py
+++ elevator.py
@@ -3,26 +3,35 @@ from boxlift_api import Command
 
 class Elevator:
     def __init__(self, el_id):
-        self.direction = 0
+        self.cur_direction = 0
+        self.next_direction = 0
         self.location = 0
         self.speed = 0
         self.stops = []
         self.el_id = el_id
 
+    def process_request(self, request):
+        """Handles a request, returns True if it will, Flase otherwise"""
+        will_do = self.go_to(request['floor'])
+        if will_do:
+            self.next_direction = request['direction']
+
+        return will_do
+
     def go_to(self, floor):
         """Put the elevator on a course for a new floor"""
         # TODO: handle buttons first
-        if self.direction == -1 and floor > self.location:
+        if self.cur_direction == -1 and floor > self.location:
             return False
-        elif self.direction == 1 and floor < self.location:
+        elif self.cur_direction == 1 and floor < self.location:
             return False
 
         self.process_buttons([floor])
         return True
 
     def sort_stops(self):
-        """Sorts the stops in the current direction order"""
-        self.stops = sorted(self.stops, reverse=(self.direction == -1))
+        """Sorts the stops in the current cur_direction order"""
+        self.stops = sorted(self.stops, reverse=(self.cur_direction == -1))
 
     def process_buttons(self, buttons):
         """Get a union between the stops we have currenty and the newly reported
@@ -39,11 +48,12 @@ class Elevator:
         """Returns a command object for sending to the API"""
         self.speed = 1
         if self.stops[-1] > self.location:
-            self.direction = 1
+            self.cur_direction = 1
         elif self.stops[-1] < self.location:
-            self.direction = -1
+            self.cur_direction = -1
         else:
-            self.direction = 0
+            self.cur_direction = self.next_direction
+            self.next_direction = 0
             self.speed = 0
 
-        return Command(self.el_id, self.direction, self.speed)
+        return Command(self.el_id, self.cur_direction, self.speed)
--- tests/test_elevator.py
+++ tests/test_elevator.py
@@ -3,23 +3,27 @@ from nose.tools import assert_equals
 
 
 class TestElevator:
+    """Test the Elevator class and its internal methods"""
     def setup(self):
-        self.elevator = Elevator(1)
+        self.elevator = Elevator(0)
 
     def test_go_to_up(self):
+        """Elevator will go up"""
         will_do_it = self.elevator.go_to(2)
         assert self.elevator.stops[-1] == 2
         assert will_do_it is True
 
     def test_get_command(self):
+        """Elevator will give the correct command object"""
         self.elevator.go_to(2)
         comm = self.elevator.get_command()
 
         assert comm.direction == 1
-        assert comm.id == 1
+        assert comm.id == 0
         assert comm.speed == 1
 
     def test_process_buttons(self):
+        """Will take buttons in and set up the next stops"""
         buttons = [2, 3]
         self.elevator.process_buttons(buttons)
         assert self.elevator.stops == buttons
@@ -29,6 +33,7 @@ class TestElevator:
         assert self.elevator.stops == [2, 3, 4]
 
     def test_update_state_no_buttons(self):
+        """Takes in a state object with no buttons"""
         state = {
             'id': 1,
             'floor': 2
@@ -38,6 +43,7 @@ class TestElevator:
         assert self.elevator.location == state['floor']
 
     def test_update_state_with_buttons(self):
+        """Takes in a state object with buttons"""
         state = {
             'id': 1,
             'floor': 2,
@@ -49,6 +55,7 @@ class TestElevator:
         assert self.elevator.stops == state['buttons_pressed']
 
     def test_update_state_with_more_buttons(self):
+        """Updates the elevator state by adding more buttons"""
         self.elevator.process_buttons([1, 2])
         state = {
             'id': 1,
@@ -61,6 +68,7 @@ class TestElevator:
         assert self.elevator.stops == [1, 2, 3]
 
     def test_update_state_with_high_dest(self):
+        """Updating state with a higher destination updates buttons"""
         self.elevator.go_to(5)
         self.elevator.process_buttons([5, 6])
 
@@ -68,10 +76,11 @@ class TestElevator:
         assert_equals(self.elevator.stops[-1], 6)
 
     def test_go_to_down(self):
+        """Elevator will go to a lower floor"""
         self.elevator.location = 5
         self.elevator.go_to(3)
         comm = self.elevator.get_command()
-        assert comm.id == 1
+        assert comm.id == 0
         assert comm.direction == -1
         assert comm.speed == 1
 
@@ -81,3 +90,37 @@ class TestElevator:
         self.elevator.process_buttons([9])
         self.elevator.get_command()
         assert self.elevator.go_to(4) is False
+
+    def test_process_request(self):
+        """Handle a request object"""
+        request = {
+            "direction": 1,
+            "floor": 5
+        }
+        will_do = self.elevator.process_request(request)
+        assert will_do is True
+
+    def test_elevator_request_up(self):
+        """Works how I expect it to work"""
+        cur_floor = 0
+        while self.elevator.location < 5:
+            request = {
+                "direction": 1,
+                "floor": 5
+            }
+            res = self.elevator.process_request(request)
+            assert res is True
+            comm = self.elevator.get_command()
+            assert comm.direction == 1
+            assert comm.speed == 1
+            assert self.elevator.next_direction == 1
+            cur_floor += 1
+            state = {
+                "floor": cur_floor,
+                "id": 0
+            }
+            self.elevator.update_state(state)
+
+        comm = self.elevator.get_command()
+        assert comm.direction == 1
+        assert self.elevator.speed == 0
